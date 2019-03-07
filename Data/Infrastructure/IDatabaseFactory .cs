91e7645efc66667fa2a6448bc0da73a2ca839d74
mohamed karim sleimi <karim-nar@live.fr>
2019-03-07 17:50:29 +0100

    3/7/2019::17:50

---
+++ Data/Infrastructure/IDatabaseFactory .cs
@@ -0,0 +1,15 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public interface IDatabaseFactory : IDisposable
+    {
+        DatabContext DataContext { get; }
+    }
+
+}
--- Data/Migrations/201903071542436_mj1.Designer.cs
+++ Data/Migrations/201903071542436_mj1.Designer.cs
@@ -7,13 +7,13 @@ namespace Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class first : IMigrationMetadata
+    public sealed partial class mj1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(first));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(mj1));
         
         string IMigrationMetadata.Id
         {
-            get { return "201902250148229_first"; }
+            get { return "201903071542436_mj1"; }
         }
         
         string IMigrationMetadata.Source
